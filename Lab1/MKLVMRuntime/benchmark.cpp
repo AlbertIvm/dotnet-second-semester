0f556649473be7277a0b5ccb1b0dd454cfa9b3bc
AlbertIvm <smyaosan@gmail.com>
2022-05-31 16:40:53 +0300

    Add C++ dummy code

--- Lab1/MKLVMRuntime/benchmark.cpp
+++ Lab1/MKLVMRuntime/benchmark.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 
-#include <iostream>;
-#include <chrono>;
+#include <iostream>
+#include <chrono>
 
 #include "mkl.h"
 
@@ -12,18 +12,31 @@ enum VMf : MKL_INT {
 };
 
 extern "C" _declspec(dllexport)
-void CallMKLFunction(int function_code, int n, double *points, double *timings,
+void CallMKLFunction(int function_code, const MKL_INT n, const double *points, double *timings,
 				     double &max_error, double &max_error_arg, double *max_error_func_values, int& ret)
 {
+	double* results = new double[n];
+	
+	// TODO: find out why calls are failing and add proper code here
 	switch (function_code) {
 	case VMf::Sin:
-		std::cout << "Calling Sin" << std::endl;
 		break;
 	case VMf::Cos:
-		std::cout << "Calling Cos" << std::endl;
 		break;
 	case VMf::SinCos:
-		std::cout << "Calling SinCos" << std::endl;
 		break;
 	}
+
+	// TODO: Dummy code, remove later
+	for (size_t i = 0; i < 3; i++) {
+		timings[i] = (2 - i) * n / 2;
+	}
+	max_error = (double) n;
+	max_error_arg = n > 0 ? n - 1 : 0;
+	for (size_t i = 0; i < 3; i++) {
+		max_error_func_values[i] = i * n / 2;
+	}
+	ret = 0;
+	
+	delete[] results;
 }
--- Lab1/TestbenchTerminal/Program.cs
+++ Lab1/TestbenchTerminal/Program.cs
@@ -7,14 +7,21 @@ namespace TestbenchTerminal
     {
         static void Main(string[] args)
         {
-            double[] points = new double[] { 0.0, 1.0 };
+            double[] points = new double[] { 0.0, Math.PI / 2, Math.PI };
             double[] timings = new double[points.Length];
             double maxError, maxErrorArg;
             double[] maxErrorFuncValues = new double[3];
             int retCode;
-            MKLWrapper.VMBenchmark.CallMKLFunction(
-                    MKLWrapper.VMf.Cos, 2, points, timings,
-                    out maxError, out maxErrorArg, maxErrorFuncValues, out retCode);
+            try
+            {
+                MKLWrapper.VMBenchmark.CallMKLFunction(
+                        MKLWrapper.VMf.Cos, points.Length, points, timings,
+                        out maxError, out maxErrorArg, maxErrorFuncValues, out retCode);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"I'm tired... exceptionally: {e}");
+            }
             Console.WriteLine("Hello World!");
         }
     }
