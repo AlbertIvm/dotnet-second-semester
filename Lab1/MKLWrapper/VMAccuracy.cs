b8fee38d4b6ca13c7683370aee27b66f98507c54
AlbertIvm <smyaosan@gmail.com>
2022-09-03 10:48:57 +0300

    Add functionality to data types

--- Lab1/MKLWrapper/VMAccuracy.cs
+++ Lab1/MKLWrapper/VMAccuracy.cs
@@ -3,9 +3,22 @@
     public struct VMAccuracy
     {
         public VMGrid Grid { get; set; }
+        public VMf FunctionType { get; set; }
+        public double MaxAbsError { get; set; }
+        public double MaxAbsErrorArgument { get; set; }
+        public double MaxAbsErrorValueHa { get; set; }
+        public double MaxAbsErrorValueLa { get; set; }
+        public double MaxAbsErrorValueEp { get; set; }
+
         public override string ToString()
         {
-            return Grid.ToString();
+            return $"VMAccuracy properties: grid: {Grid.ToString()}, " +
+                   $"function type: {FunctionType.ToString()}, " +
+                   $"maximum absolute error: {MaxAbsError}, " +
+                   $"reached at {MaxAbsErrorArgument} with function values being " +
+                   $"{MaxAbsErrorValueHa} for HA, " +
+                   $"{MaxAbsErrorValueLa} for LA " +
+                   $"and {MaxAbsErrorValueEp} for EP modes. ";
         }
     }
 }
--- Lab1/MKLWrapper/VMTime.cs
+++ Lab1/MKLWrapper/VMTime.cs
@@ -3,9 +3,21 @@
     public struct VMTime
     {
         public VMGrid Grid { get; set; }
+        public VMf FunctionType { get; set; }
+        public double CalcTimeHA { get; set; }
+        public double CalcTimeLA { get; set; }
+        public double CalcTimeEP { get; set; }
+        public double LaToHaTimingRatio { get; set; }
+        public double EpToHaTimingRatio { get; set; }
+
         public override string ToString()
         {
-            return base.ToString();
+            return $"VMTime properties: grid: {Grid.ToString()}; " +
+                   $"function type: {FunctionType.ToString()}; " +
+                   $"calculation times (in ms) for modes: HA: {CalcTimeHA}, " +
+                   $"LA: {CalcTimeLA}, EP: {CalcTimeEP}; " +
+                   $"LA calculation taking {LaToHaTimingRatio} of HA's time " +
+                   $"and EP calculation taking {EpToHaTimingRatio} of HA's time.";
         }
     }
 }
