878ad052aa0e1abdc3843429837072ae4db0beea
AlbertIvm <smyaosan@gmail.com>
2022-09-15 02:48:27 +0300

    Add info about current selected element

--- Lab1/MKLVMApplication/Converters.cs
+++ Lab1/MKLVMApplication/Converters.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Data;
 
 namespace MKLBenchmarkApp
@@ -37,6 +38,72 @@ namespace MKLBenchmarkApp
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             return $"Least EP to HA timings' ratio in benchmark: {value}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    [ValueConversion(typeof(MKLWrapper.VMTime), typeof(string))]
+    internal class VMTimeExtraInfoStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                MKLWrapper.VMTime vmtime = (MKLWrapper.VMTime)value;
+                return $"EP to HA timings' ratio: {vmtime.EpToHaTimingRatio:0.####};\nLA to HA timings' ratio: {vmtime.LaToHaTimingRatio:0.####}";
+            }
+            catch (NullReferenceException)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    [ValueConversion(typeof(MKLWrapper.VMTime), typeof(string))]
+    internal class VMAccuracyMaxErrorArgStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                MKLWrapper.VMAccuracy vmaccuracy = (MKLWrapper.VMAccuracy)value;
+                return $"Point at which the error is maximal: {vmaccuracy.MaxAbsErrorArgument}";
+            }
+            catch (NullReferenceException)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+
+    [ValueConversion(typeof(MKLWrapper.VMTime), typeof(string))]
+    internal class VMAccuracyMaxErrorValuesStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                MKLWrapper.VMAccuracy vmaccuracy = (MKLWrapper.VMAccuracy)value;
+                return $"Function's values at this point:\nHA: {vmaccuracy.MaxAbsErrorValueHa},\nLA: {vmaccuracy.MaxAbsErrorValueLa},\nEP: {vmaccuracy.MaxAbsErrorValueEp}";
+            }
+            catch (NullReferenceException)
+            {
+                return DependencyProperty.UnsetValue;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
